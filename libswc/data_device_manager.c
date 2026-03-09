2bbe725ccd4dea8bed3dd06ebd34ce523a896021
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-09 15:13:55 +0200

    data_device_manager: add docs

--- libswc/data_device_manager.c
+++ libswc/data_device_manager.c
@@ -1,7 +1,30 @@
-/* swc: data_device_manager.c
+/**
+ * \file data_device_manager.c
+ * \brief Implementation of the Wayland wl_data_device_manager global.
+ * \author Michael Forney
+ * \date 2013--2020
+ * \copyright MIT
  *
- * Copyright (c) 2013-2020 Michael Forney
+ * \details
+ * This module provides the compositor-side implementation of the
+ * wl_data_device_manager interface used for clipboard and drag-and-drop data
+ * transfers between Wayland clients.
  *
+ * Clients bind to this global through the Wayland registry and may then:
+ *
+ *  - create wl_data_source objects to offer transferable data
+ *  - obtain wl_data_device objects associated with a wl_seat
+ *
+ * The manager acts as the entry point for all clipboard and drag-and-drop
+ * operations within the compositor.
+ *
+ * Most functionality is implemented in:
+ *
+ *  - \c data_source_new() - creation of data sources
+ *  - \c data_device_bind() - client binding to seat data devices
+ */
+
+/*
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -27,40 +50,95 @@
 #include "internal.h"
 #include "seat.h"
 
+/**
+ * \brief Handle wl_data_device_manager.create_data_source.
+ * \details
+ * Creates a new wl_data_source object owned by the requesting client.
+ * The data source represents a provider of clipboard or drag-and-drop
+ * data that may later be offered to other clients.
+ *
+ * \param[in] client Client issuing the request.
+ * \param[in] resource wl_data_device_manager resource used for the request.
+ * \param[in] id Object ID for the newly created wl_data_source.
+ */
 static void
 create_data_source(struct wl_client *client, struct wl_resource *resource, uint32_t id)
 {
-	if (!data_source_new(client, wl_resource_get_version(resource), id))
+	const uint32_t version = wl_resource_get_version(resource);
+	if (!data_source_new(client, version, id))
 		wl_resource_post_no_memory(resource);
 }
 
+/**
+ * \brief Handle wl_data_device_manager.get_data_device.
+ * \details
+ * Returns a wl_data_device associated with the specified wl_seat. The data
+ * device allows the client to participate in clipboard and drag-and-drop
+ * operations for that seat.
+ *
+ * \param[in] client Client issuing the request.
+ * \param[in] resource wl_data_device_manager resource used for the request.
+ * \param[in] id Object ID for the created wl_data_device.
+ * \param[in] seat_resource wl_seat identifying the seat whose data device
+ * should be bound.
+ */
 static void
 get_data_device(struct wl_client *client, struct wl_resource *resource, uint32_t id, struct wl_resource *seat_resource)
 {
+	const uint32_t version = wl_resource_get_version(resource);
 	struct swc_seat *seat = wl_resource_get_user_data(seat_resource);
 
-	if (!data_device_bind(seat->data_device, client, wl_resource_get_version(resource), id))
+	if (!data_device_bind(seat->data_device, client, version, id))
 		wl_resource_post_no_memory(resource);
 }
 
+/** Request handler implementation for wl_data_device_manager. */
 static const struct wl_data_device_manager_interface data_device_manager_impl = {
 	.create_data_source = create_data_source,
 	.get_data_device = get_data_device,
 };
 
+/**
+ * \brief Bind the wl_data_device_manager global to a client.
+ * \details
+ * Called by the Wayland server when a client binds the wl_data_device_manager
+ * global advertised in the registry.
+ *
+ * A wl_resource representing the manager interface is created and associated
+ * with the request handler implementation.
+ *
+ * \param[in] client Client binding the global.
+ * \param[in] data User data associated with the global (unused).
+ * \param[in] version Protocol version requested by the client.
+ * \param[in] id Object ID for the created wl_data_device_manager resource.
+ */
 static void
 bind_data_device_manager(struct wl_client *client, void *data, uint32_t version, uint32_t id)
 {
 	struct wl_resource *resource;
+
+	(void)data;
 
 	resource = wl_resource_create(client, &wl_data_device_manager_interface, version, id);
 	if (!resource) {
 		wl_client_post_no_memory(client);
 		return;
 	}
+
 	wl_resource_set_implementation(resource, &data_device_manager_impl, NULL, NULL);
 }
 
+/**
+ * \brief Create the wl_data_device_manager global.
+ * \details
+ * Registers the wl_data_device_manager interface with the Wayland display so
+ * that clients can bind to it through the registry.
+ *
+ * The global is advertised with protocol version 2.
+ *
+ * \param[in] display Wayland display used to register the global.
+ * \return The created wl_global object.
+ */
 struct wl_global *
 data_device_manager_create(struct wl_display *display)
 {
--- libswc/data_device_manager.h
+++ libswc/data_device_manager.h
@@ -1,7 +1,39 @@
-/* swc: data_device_manager.h
+/**
+ * \file data_device_manager.h
  *
- * Copyright (c) 2013-2019 Michael Forney
+ * \brief Wayland data device manager interface for swc.
  *
+ * \author Michael Forney
+ * \date 2013–2019
+ * \copyright MIT
+ *
+ * \details
+ * This module exposes the compositor-side implementation of the Wayland \c
+ * wl_data_device_manager global interface.
+ *
+ * The data device manager is responsible for coordinating data transfer
+ * mechanisms between Wayland clients, including:
+ *
+ * - Clipboard (selection) handling
+ * - Drag-and-drop data exchange
+ *
+ * The interface is advertised to clients through the Wayland registry as a
+ * global object. Clients bind to this global during initialization and may
+ * subsequently:
+ *
+ * - Create \c wl_data_source objects representing transferable data.
+ * - Obtain \c wl_data_device objects associated with a specific \c wl_seat.
+ *
+ * Each seat in the compositor maintains a data device responsible for handling
+ * selection ownership and drag-and-drop interactions between clients.
+ *
+ * The compositor initializes this functionality by creating a single data
+ * device manager global using ::data_device_manager_create().
+ *
+ * \see https://wayland.freedesktop.org/docs/html/apa.html#protocol-spec-wl_data_device_manager
+ */
+
+/*
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -26,6 +58,17 @@
 
 struct wl_display;
 
+/**
+ * \brief Create the wl_data_device_manager global.
+ * \details
+ * Registers the wl_data_device_manager interface with the Wayland display so
+ * that clients can bind to it through the registry.
+ *
+ * The global is advertised with protocol version 2.
+ *
+ * \param[in] display Wayland display used to register the global.
+ * \return The created wl_global object.
+ */
 struct wl_global *data_device_manager_create(struct wl_display *display);
 
 #endif
